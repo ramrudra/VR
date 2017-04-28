1a46d0c49651cbc41d78df8fb374cff2431c58e6
ramrudra <ramrudra.p@gmail.com>
2017-04-28 17:15:55 +0530

    Inital Commit

---
+++ tests/aa.js
@@ -0,0 +1,22 @@
+var config = require('./config.json');
+console.log('URL:', config.url);
+
+var phantomcss = require('phantomcss');
+
+casper.test.begin('Tags', function(test) {
+
+  phantomcss.init({
+    rebase: casper.cli.get('rebase')
+  });
+
+  // open page
+  casper.start();
+
+  casper.thenOpen('https://www.facebook.com', function() {
+    console.log("URL open");
+  });
+ 
+});
+
+
+
